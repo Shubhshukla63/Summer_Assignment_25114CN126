cf7fa2f079e9048d11572e8e7e62b757bf380504
Shubhshukla63 <shubhshukla6388@gmail.com>
2026-07-10 08:24:57 +0530

    updated hello.c

--- hello.c
+++ hello.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
 int main () { 
+    int n, sum = 0;
 
-    printf("Hello Shubh");
+    printf("enter a number");
+    scanf("%d", &n);
 
+    for(int i = 1; i <= n; ++i) {
+        sum += i;
+    }
+
+    printf("sum = %d", sum);
     return 0;
 
 }
